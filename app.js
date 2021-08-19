af5d356995f81de0fbda0083021d9fbe375c30e0
Daryl-webdev <grimaldomarkdaryl@gmail.com>
2021-08-19 20:01:26 +0800

    Add dbconnection to env file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 
 //==================================Data base connection========================================
-mongoose.connect("mongodb+srv://daryl:marklyrad24@grimaldo-b121.pnyea.mongodb.net/todolist-app",{
+mongoose.connect(process.env.DB_CONNECTION,{
   useNewUrlParser: true,
   useUnifiedTopology: true
  })
